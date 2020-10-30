c31b1f888067f3ee6bff2e435369e2453a2a42b1
moying <moying.wu@gzgi.com>
2020-10-30 14:36:51 +0800

    环境初始搭建

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+    meta:{
+      index:1// 添加 meta 属性，约定 1 为第一级
+    }
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: {
+      index: 2 // 添加 meta 属性，约定 2 为第二级
+    }
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <van-button type="primary">默认按钮</van-button>
+    <div class="hi">你好</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="less" scoped>
+  .hi{
+    color:red;
+    background: yellow;
+    width: 75px;
+    height: 75px;
+    font-size: 18PX;
+  }
+</style>
