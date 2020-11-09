0257de6b3efac4118968c782d3b9ee2a444c98e3
moying <moying.wu@gzgi.com>
2020-11-09 18:32:23 +0800

    vuex练习

--- src/views/study/Freshen.vue
+++ src/views/study/Freshen.vue
@@ -1,6 +1,6 @@
 <template>
   <van-pull-refresh v-model="isLoading" success-text="刷新成功" @refresh="onRefresh">
-    <p>刷新次数: {{ count }}</p>
+    <p>刷新次数: {{ getcount }}</p>
     <!-- 下拉提示，通过 scale 实现一个缩放效果 -->
     <img
       class="doge"
@@ -21,16 +21,22 @@
       slot="loading"
       src="https://img.yzcdn.cn/vant/doge-fire.jpg"
     >
+    <div v-text="count"></div>
+    <div>doneTodosCount{{doneTodosCount}}</div>
   </van-pull-refresh>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import { mapGetters } from 'vuex'
+import { mapMutations } from 'vuex'
+import { mapActions } from 'vuex'
 import { Toast } from 'vant';
 
 export default {
   data() {
     return {
-      count: 0,
+      getcount: 0,
       isLoading: false
     }
   },
@@ -40,9 +46,35 @@ export default {
       setTimeout(() => {
         Toast('刷新成功');
         this.isLoading = false;
-        this.count++;
+        // this.$store.commit('incrementAsync',{
+        //   amount:10
+        // })
+        // this.incrementAsync({
+        //   amount:10
+        // })
+        this.increment()
+        console.log(this.$store.state.count)
+        console.log("onRefresh -> this.store.getters.getTodoById(2)", this.$store.getters.getTodoById(2))
+        this.getcount++;
       }, 1000);
-    }
+    },
+    ...mapMutations([
+      'increment'
+    ]),
+    ...mapActions([
+      'increment'
+    ])
+  },
+  computed:mapState({
+    count:function(){
+      return this.$store.state.count
+    },
+    ...mapGetters([
+      'doneTodosCount'
+    ])
+  }),
+  created(){
+    console.log(this.$store.getters.doneTodosCount)
   }
 }
 </script>
